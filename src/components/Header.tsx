9f17af0f3c348321fbe9d024403613b4b7edfa6e
Raki <killer.vp.mic@gmail.com>
2023-03-07 18:08:11 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+type Props = {}
+
+export const Header: FC<Props> = (props) => {
+    return (
+        <header className='flex justify-center items-center h-20 w-full bg-emerald-400'>
+
+        </header>
+    )
+}
