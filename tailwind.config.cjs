c433219b6b1137d36252497016ff2df71197b616
Raki <killer.vp.mic@gmail.com>
2023-03-08 21:35:21 -0300

    added discord theme to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blurple: "#7289DA",
+        "full-white": "#FFFFFF",
+        greypurple: "#99AAB5",
+        "dark-not-black": "#2C2F33",
+        "not-quite-black": "#23272A",
+        green: "#43B581",
+        yellow: "#FAA61A",
+        red: "#F04747",
+        background: "#404eed",
+      },
+    },
   },
   plugins: [],
 }
