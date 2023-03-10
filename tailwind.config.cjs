208eaf7638026882b4218cf560f7b8ac544b8865
Raki <killer.vp.mic@gmail.com>
2023-03-10 11:47:36 -0300

    added fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'ginto-nord': ['Ginto-Nord', 'sans-serif'],
+      'gg-sans': ['gg-sans', 'sans-serif'],
+    },
     extend: {
       colors: {
         blurple: "#7289DA",
+        blurpleButton: "#5865F2",
+        blurpleHover: "#7983f5",
         "full-white": "#FFFFFF",
         greypurple: "#99AAB5",
         "dark-not-black": "#2C2F33",
