6d9cd5aef08878018bf4ed2d37c7b45e088c255c
Raki <killer.vp.mic@gmail.com>
2023-03-10 11:52:23 -0300

    updated hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { Button } from './Header';
+import DownloadButton from './svg/DownloadButton';
 
 type Props = {}
 
@@ -17,19 +18,20 @@ export const Hero: FC<Props> = (props) => {
                 xl:col-start-3 xl:col-end-11"
             >
                 <div className="">
-                    <h1 className="text-5xl">
+                    <h1 className="text-5xl font-ginto-nord">
                         IMAGINE A PLACE...
                     </h1>
-                    <div className="text-xl mt-10">
+                    <div className="text-xl mt-10 font-gg-sans">
                     ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.
                     </div>
                 </div>
                 <div className="flex flex-wrap direction-column align-middle
                  lg:justify-center">
-                    <Button className='bg-white text-black rounded-full cursor-pointer hover:text-blurple hover:shadow-xl
+                    <Button className='flex items-center justify-center bg-white text-dark-not-black font-medium rounded-full cursor-pointer hover:text-blurple hover:shadow-xl
                      transition duration-200 ease-in-out
                      mr-6 mt-6  px-8 py-4'
                     >
+                        <DownloadButton className='inline mr-2 z-10' />
                         Download for Windows
                     </Button>
                     <Button className='bg-not-quite-black rounded-full text-white cursor-pointer 
